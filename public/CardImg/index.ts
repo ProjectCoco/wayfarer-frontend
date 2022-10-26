4c58548df5ac03425a46ed2b0655e61753f6ef76
JEONGHWANMIN <mimi0j0j@naver.com>
2022-10-26 21:33:35 +0900

    feat : 이미지 파일 추가

---
+++ public/CardImg/index.ts
@@ -0,0 +1,11 @@
+import ProjectVerticalImg from "./ProjectVerticalImg.png";
+import StudyVerticalImg from "./StudyVerticalImg.png";
+import ProjectHorizontalImg from "./ProjectHorizontalImg.png";
+import StudyHorizontalImg from "./StudyHorizontalImg.png";
+
+export const CardImg = {
+  ProjectVerticalImg,
+  StudyVerticalImg,
+  ProjectHorizontalImg,
+  StudyHorizontalImg,
+};
---
+++ public/TechStackImg/index.ts
@@ -0,0 +1,11 @@
+import django from "./django.png";
+import vue from "./vue.png";
+import figma from "./figma.svg";
+
+export const TechStackImg = {
+  django,
+  vue,
+  figma,
+};
+
+export type TechStackImgType = keyof typeof TechStackImg;

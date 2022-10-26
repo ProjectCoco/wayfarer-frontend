08c134224d47311a5a96e967fa2b2e707498885a
JEONGHWANMIN <mimi0j0j@naver.com>
2022-10-26 19:30:06 +0900

    feat : 테마 프로바이더 및 테마 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import type { AppProps } from "next/app";
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "../styles/DefaultTheme";
 import GlobalStyle from "../styles/GlobalStyle";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <GlobalStyle />
-      <Component {...pageProps} />
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ThemeProvider>
     </>
   );
 }
--- styles/styled.d.ts
+++ styles/styled.d.ts
@@ -1,13 +1,41 @@
 import "styled-components";
+import { defaultTheme } from "./DefaultTheme";
+
+type DefaultColor = typeof defaultTheme["colors"];
 
 declare module "styled-components" {
   export interface DefaultTheme {
     colors: {
-      [key: string]: string;
+      Blue: "#001AFF";
+      sub_blue: "#72B8FF";
+      sub_blue1: "#C0CEFF";
+      sub_blue2: "#97AEFF";
+      sub_blue3: "#6B8CFF";
+      sub_blue4: "#4770FF";
+      sub_blue5: "#1F8EFF";
+      sub_blue6: "#72B8FF";
+      // yellow
+      Yello: "#E7FE54";
+      sub_yellow: "#FFEF5A";
+      // green
+      green: "#7AFFCD";
+      // gray
+      gray_100: "#FAFAFA";
+      gray_200: "#F2F2F3";
+      gray_300: "#E6E6E8";
+      gray_400: "#BDBEC1";
+      gray_500: "#96989C";
+      gray_600: "#74767B";
+      gray_700: "#4A4B4F";
+      gray_800: "#2D2D2F";
+      gray_900: "#000000";
     };
 
     fontSize: {
-      [key: string]: string;
+      heading6_text_6xl_regular: "32px";
+      caption1_text_xs_regular: "10px";
+      caption2_text_2xs_regular: "11px";
+      caption3_text_3xs_regular: "12px";
     };
   }
 }

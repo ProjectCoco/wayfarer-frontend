7b0bd7cf5ff5c8142b2bc64b313c40830e80910d
JEONGHWANMIN <mimi0j0j@naver.com>
2022-11-01 02:43:39 +0900

    Feat : svg 이미지 추가

--- public/CardImg/index.ts
+++ public/CardImg/index.ts
@@ -2,10 +2,12 @@ import ProjectVerticalImg from "./ProjectVerticalImg.png";
 import StudyVerticalImg from "./StudyVerticalImg.png";
 import ProjectHorizontalImg from "./ProjectHorizontalImg.png";
 import StudyHorizontalImg from "./StudyHorizontalImg.png";
+import AdCardImg from "./AdCardImg.svg";
 
 export const CardImg = {
   ProjectVerticalImg,
   StudyVerticalImg,
   ProjectHorizontalImg,
   StudyHorizontalImg,
+  AdCardImg,
 };

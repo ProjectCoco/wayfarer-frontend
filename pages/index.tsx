1fc322c1c5a503225035c5bffcdbeac4bcd9f8a7
seonnn <ssni0105@gmail.com>
2022-11-25 17:29:05 +0900

    refactor: 디자인 변경으로 인한 불필요한 코드 제거

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,50 +1,13 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import RecruitToggle from "../components/common/RecruitToggle";
 import styled from "styled-components";
-import LandingCard from "../components/common/LandingCard";
-import StudyCard from "../components/common/StudyCard";
-import SubBanner from "../components/pages/Home/SubBanner";
-import TitleAndSubTitle from "../components/pages/Home/TitleAndSubTitle";
+
 import Banner from "../components/common/Banner";
 
 const Home: NextPage = () => {
   return (
     <HomeContainer>
       <Banner />
-      <ContentsContainer>
-        <PopularContainer>
-          <TitleAndSubTitle title="지금 뜨는 사이드프로젝트 보러갈까요? 🚀" />
-          <LandingCards>
-            <LandingCard />
-            <LandingCard />
-          </LandingCards>
-        </PopularContainer>
-        <StudyContainer>
-          <TitleAndSubTitle title="따끈 따끈 신규스터디, 지금 바로 지원하세요! 👈" />
-          <LandingCards>
-            <LandingCard />
-            <LandingCard />
-          </LandingCards>
-        </StudyContainer>
-        <ProjectContainer>
-          <TitleAndSubTitle title="이런 프로젝트, 어떠세요? 🥰" />
-          <LandingCards>
-            <StudyCard />
-            <StudyCard />
-            <StudyCard />
-          </LandingCards>
-          <SubBannerWrapper>
-            <SubBanner />
-          </SubBannerWrapper>
-          <LandingCards>
-            <StudyCard />
-            <StudyCard />
-            <StudyCard />
-          </LandingCards>
-        </ProjectContainer>
-      </ContentsContainer>
+      <ContentsContainer></ContentsContainer>
     </HomeContainer>
   );
 };
@@ -66,33 +29,3 @@ const ContentsContainer = styled.div`
   width: 1200px;
   margin-top: 80px;
 `;
-
-const PopularContainer = styled.section`
-  margin-bottom: 135px;
-  & > div:nth-child(1) {
-    margin-bottom: 66px;
-  }
-`;
-const StudyContainer = styled.section`
-  margin-bottom: 108px;
-  & > div:nth-child(1) {
-    margin-bottom: 66px;
-  }
-`;
-const ProjectContainer = styled.section`
-  & > div:nth-child(1) {
-    margin-bottom: 40px;
-  }
-`;
-
-const LandingCards = styled.div`
-  display: flex;
-  width: 100%;
-  justify-content: space-between;
-`;
-
-const SubBannerWrapper = styled.div`
-  margin: 80px 0;
-`;
-
-const NewStudy = styled.div``;
--- pages/study/index.tsx
+++ pages/study/index.tsx
@@ -1,11 +1,6 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import RecruitToggle from "../../components/common/RecruitToggle";
-import StudyCard from "../../components/common/StudyCard";
-import TechNameTag from "../../components/common/TechNameTag";
 import { TabMenus, TabTechStacks, TabMenuType } from "../../utils/studyTabMenu";
-import Image from "next/image";
-import BannerImg from "../../public/Banner/Banner.svg";
 import Banner from "../../components/common/Banner";
 
 function Study() {
@@ -25,45 +20,7 @@ function Study() {
   return (
     <StudyContainer>
       <Banner />
-      <InnerContainer>
-        <RecruitBox>
-          <RecruitTitle>모집 중</RecruitTitle>
-          <RecruitToggle toggle={toggle} setToggle={setToggle} />
-        </RecruitBox>
-        <TabMenuBox>
-          {TabMenus.map((menu) => (
-            <TabMenu
-              key={menu}
-              onClick={() => {
-                setTabMenu(menu);
-                setCurrentTag(TabTechStacks[menu]);
-                console.log(TabTechStacks[menu]);
-              }}
-              active={tabMenu === menu}
-            >
-              {menu}
-            </TabMenu>
-          ))}
-        </TabMenuBox>
-        <TechTagNameBox>
-          {Object.keys(currentTag).map((tag) => (
-            <TechNameTag
-              key={tag}
-              tagName={tag}
-              active={currentTag[tag]}
-              tagActivate={tagActivate}
-            />
-          ))}
-        </TechTagNameBox>
-        <StudyCards>
-          <StudyCard />
-          <StudyCard />
-          <StudyCard />
-          <StudyCard />
-          <StudyCard />
-          <StudyCard />
-        </StudyCards>
-      </InnerContainer>
+      <InnerContainer></InnerContainer>
     </StudyContainer>
   );
 }
@@ -82,48 +39,4 @@ const InnerContainer = styled.div`
   margin-top: 80px;
   display: flex;
   flex-direction: column;
-`;
-
-const RecruitBox = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 11.8px;
-`;
-
-const RecruitTitle = styled.h3`
-  font-weight: 700;
-  font-size: 24px;
-  line-height: 30px;
-  color: ${({ theme }) => theme.colors.gray_700};
-`;
-
-const TabMenuBox = styled.div`
-  display: flex;
-  gap: 84px;
-  margin-top: 27px;
-`;
-
-const TabMenu = styled.h2<{ active: boolean }>`
-  font-weight: 400;
-  font-size: 30px;
-  line-height: 38px;
-  color: ${({ theme, active }) =>
-    active ? theme.colors.gray_700 : theme.colors.gray_500};
-  cursor: pointer;
-  white-space: nowrap;
-`;
-
-const TechTagNameBox = styled.div`
-  margin-top: 49px;
-  display: flex;
-  flex-wrap: wrap;
-  gap: 64px;
-`;
-
-const StudyCards = styled.section`
-  margin-top: 80px;
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  grid-gap: 41px;
-  margin-bottom: 80px;
 `;

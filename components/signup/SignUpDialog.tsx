baac62aa6a5a429a92fa9f5a2d228ca8afbc60cf
JEONGHWANMIN <mimi0j0j@naver.com>
2022-11-30 00:16:40 +0900

    feat : 커스텀 이벤트 훅 추가

--- components/signup/SignUpDialog.tsx
+++ components/signup/SignUpDialog.tsx
@@ -1,36 +1,66 @@
 import React from "react";
 import styled from "styled-components";
-import CheckBox from "../common/Input/CheckBox";
+import useChecked from "../../hooks/Signup/useChecked";
+import useSignUpForm from "../../hooks/Signup/useSignupForm";
 import DialogHeader from "../signin/DialogHeader";
 import LabelCheck from "./LabelCheck";
 import LabelInput from "./LabelInput";
 import SubmitButton from "./SubmitButton";
 
 function SignUpDialog() {
+  const { form, handleInputChange, handleSubmit } = useSignUpForm();
+  const { check, handleCheck, handleAllCheck } = useChecked();
+  console.log(form);
   return (
     <>
       <DialogHeader
         title="회원가입"
         subTitle="잠깐, 몇가지 정보가 더 필요해요 :)"
       />
-      <InputBox>
-        <LabelInput label="닉네임" placeholder="별명을 입력해 주세요." />
+      <InputForm onSubmit={handleSubmit}>
+        <LabelInput
+          label="닉네임"
+          name="nickname"
+          placeholder="별명을 입력해 주세요."
+          value={form.nickname}
+          onChange={handleInputChange}
+        />
         <LabelInput label="관심분야" placeholder="관심분야를 선택해 주세요." />
-      </InputBox>
-      <CheckBoxBlock>
-        <LabelCheck label="전체동의" />
-        <LabelCheck label="(필수) 서비스 이용약관 동의" />
-        <LabelCheck label="(필수) 개인정보 수집 및 이용 동의" />
-        <LabelCheck label="(선택) 마케팅 및 이벤트 정보 수신 동의" />
-      </CheckBoxBlock>
-      <SubmitButton />
+        <CheckBoxBlock>
+          <LabelCheck
+            label="전체동의"
+            name="all"
+            checked={check.all}
+            onChange={handleAllCheck}
+          />
+          <LabelCheck
+            label="(필수) 서비스 이용약관 동의"
+            name="check1_required"
+            checked={check.check1_required}
+            onChange={handleCheck}
+          />
+          <LabelCheck
+            label="(필수) 개인정보 수집 및 이용 동의"
+            name="check2_required"
+            checked={check.check2_required}
+            onChange={handleCheck}
+          />
+          <LabelCheck
+            label="(선택) 마케팅 및 이벤트 정보 수신 동의"
+            name="check3"
+            checked={check.check3}
+            onChange={handleCheck}
+          />
+        </CheckBoxBlock>
+        <SubmitButton />
+      </InputForm>
     </>
   );
 }
 
 export default SignUpDialog;
 
-const InputBox = styled.div`
+const InputForm = styled.form`
   display: flex;
   flex-direction: column;
   gap: 45px;
@@ -41,6 +71,4 @@ const CheckBoxBlock = styled.div`
   display: flex;
   flex-direction: column;
   gap: 15px;
-  margin-top: 45px;
-  margin-bottom: 48px;
 `;

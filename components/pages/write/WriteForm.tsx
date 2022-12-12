5b9a8ab690dc386f0455d0d308649f491e3b6459
JEONGHWANMIN <mimi0j0j@naver.com>
2022-12-12 23:23:24 +0900

    feat : 모집인원 블록 추가

--- components/pages/write/WriteForm.tsx
+++ components/pages/write/WriteForm.tsx
@@ -4,6 +4,10 @@ import WriteInput from "./WriteInput";
 import Label from "./Label";
 import LabelBlock from "./LabelBlock";
 import Asterisk from "../../common/Input/Asterisk";
+import TagInput from "./TagInput";
+import TextArea from "./TextArea";
+import DropDown from "./DropDown";
+import DropDownIcon from "./DropDownIcon";
 
 function WriteForm() {
   return (
@@ -17,8 +21,20 @@ function WriteForm() {
       <LabelBlock
         label={<Label label="요약" />}
         customInput={
-          <WriteInput placeholder="프로젝트의 제목을 입력해주세요." />
+          <TagInput placeholder="#프로젝트를 키워드로 표현해주세요 (최대 5개 입력)" />
         }
+      />
+      <LabelBlock
+        label={<Label label="모집인원" icon={<Asterisk />} />}
+        customInput={<DropDown icon={<DropDownIcon />} width={"102px"} />}
+      />
+      <LabelBlock
+        label={<Label label="기술/언어" icon={<Asterisk />} />}
+        customInput={<TagInput placeholder="#기술언어를 입력해주세요." />}
+      />
+      <LabelBlock
+        label={<Label label="소개" icon={<Asterisk />} />}
+        customInput={<TextArea />}
       />
     </Form>
   );

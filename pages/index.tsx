0cee6212aa7f929883ba71dc7e88be9def0621e5
seonnn <ssni0105@gmail.com>
2022-12-13 11:32:53 +0900

    design: 홈페이지 SmallCards 레이아웃

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,18 +2,24 @@ import type { NextPage } from "next";
 import styled from "styled-components";
 import Banner from "../components/common/Banner/Banner";
 import SubBanner from "../components/common/Banner/SubBanner";
+import SmallCards from "../components/common/Cards/SmallCards";
 import BigCards from "../components/pages/Home/BigCards";
+import CardsHeader from "../components/pages/Home/CardsHeader";
 
 const Home: NextPage = () => {
   return (
     <HomeContainer>
       <Banner text={"모집글 작성하기"} />
-      <ContentsContainer>
+      <BigCardsContainer>
         <BigCards title={"지금 뜨는 사이드 프로젝트 보러 갈까요? 🚀"} />
         <BigCards title={"따끈따끈한 신규 스터디 구경해보세요 👀"} />
         <BigCards title={"지금 당신에게 필요한 인사이트 ✏️"} />
-      </ContentsContainer>
+      </BigCardsContainer>
       <SubBanner />
+      <SmallCardsContainer>
+        <CardsHeader title={"모든 사이드 프로젝트를 모아모아"} />
+        <SmallCards />
+      </SmallCardsContainer>
     </HomeContainer>
   );
 };
@@ -28,10 +34,16 @@ const HomeContainer = styled.div`
   margin-bottom: 100px;
 `;
 
-const ContentsContainer = styled.div`
+const BigCardsContainer = styled.div`
   display: flex;
   flex-direction: column;
   width: 1280px;
   margin-top: 68.23px;
   gap: 193.22px;
+`;
+
+const SmallCardsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 1280px;
 `;

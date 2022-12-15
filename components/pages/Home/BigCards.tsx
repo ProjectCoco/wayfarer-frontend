990a2cccbedf9c40a759b96cd73fb1e4dda41732
seonnn <ssni0105@gmail.com>
2022-12-15 22:03:50 +0900

    feat: BigCards component 내 BigCardCarousel 적용

--- components/pages/Home/BigCards.tsx
+++ components/pages/Home/BigCards.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import BigCard from "./BigCard";
+import BigCardCarousel from "./BigCardCarousel";
 import CardsHeader from "./CardsHeader";
 
 export interface BigCardProps {
@@ -35,42 +36,27 @@ const projectData = [
     like: true,
     id: 1,
   },
-  // {
-  //   name: "[프로젝트] 스터디",
-  //   title: "스터디 모집 서비스 '스터디'의 멤버를 모집합니다",
-  //   memeber: ["프론트엔드(1)", "백엔드(2)", "UX UI 디자이너(1)"],
-  //   tag: ["덕질", "어덕행덕", "OTT", "개발자"],
-  //   stack: ["a", "f", "k", "i", "r", "t"],
-  //   period: "11.2~12.3",
-  //   id: 2,
-  // },
+  {
+    name: "[프로젝트] 스터디",
+    title: "스터디 모집 서비스 '스터디'의 멤버를 모집합니다",
+    memeber: ["프론트엔드(1)", "백엔드(2)", "UX UI 디자이너(1)"],
+    tag: ["덕질", "어덕행덕", "OTT", "개발자"],
+    stack: ["a", "f", "k", "i", "r", "t"],
+    period: "11.2~12.3",
+    like: false,
+    id: 2,
+  },
 ];
 
 const BigCards = ({ title }: { title: string }) => {
   return (
     <LandingCardsContainer>
       <CardsHeader title={title} />
-      <CardsContents>
-        {projectData.map((data) => (
-          <BigCard key={data.id} {...data} />
-        ))}
-      </CardsContents>
+      <BigCardCarousel projectData={projectData} />
     </LandingCardsContainer>
   );
 };
 
 export default BigCards;
 
-const LandingCardsContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
-
-const CardsContents = styled.div`
-  width: 100%;
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  gap: 14px;
-`;
+const LandingCardsContainer = styled.div``;

21b32fc12317d5f4dc3f331e70253f5bd3016f8e
seonnn <ssni0105@gmail.com>
2022-12-20 22:05:32 +0900

    feat: SNS 이미지 export를 위한 index.ts 파일 추가

---
+++ public/Footer/index.ts
@@ -0,0 +1,9 @@
+import facebook from "./facebook.svg";
+import instagram from "./instagram.svg";
+import youtube from "./youtube.svg";
+
+export const SocialNetworksImg = {
+  facebook,
+  instagram,
+  youtube,
+};

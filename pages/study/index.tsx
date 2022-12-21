62a31e6f6fd984ac10c9263f74cccab116733edc
seonnn <ssni0105@gmail.com>
2022-12-22 00:55:21 +0900

    design: 스터디 페이지 레이아웃

--- pages/study/index.tsx
+++ pages/study/index.tsx
@@ -1,29 +1,27 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import { TabMenus, TabTechStacks, TabMenuType } from "../../utils/studyTabMenu";
 import Banner from "../../components/common/Banner/Banner";
+import SmallCards from "../../components/common/Cards/SmallCards";
+import OccupationFilter from "../../components/common/OccupationFilter/OccupationFilter";
+import RecruitToggle from "../../components/common/RecruitToggle/RecruitToggle";
+import { FilterMenuType } from "../../utils/Filter";
 
-function Study() {
-  const [toggle, setToggle] = useState(false);
-  const [tabMenu, setTabMenu] = useState<TabMenuType>("전체");
-  const [currentTag, setCurrentTag] = useState<any>(TabTechStacks[tabMenu]);
-
-  const tagActivate = (tagName: string) => {
-    setCurrentTag((prev: any) => {
-      return {
-        ...prev,
-        [tagName]: !prev[tagName],
-      };
-    });
-  };
-
+const Study = () => {
+  const [selectedFilter, setSelectedFilter] = useState<FilterMenuType>("전체");
   return (
     <StudyContainer>
       <Banner text={"모집글 작성하기"} />
-      <InnerContainer></InnerContainer>
+      <InnerContainer>
+        <OccupationFilter
+          selected={selectedFilter}
+          setSelectedFilter={setSelectedFilter}
+        />
+        <RecruitToggle />
+        <SmallCards />
+      </InnerContainer>
     </StudyContainer>
   );
-}
+};
 
 export default Study;
 
@@ -35,8 +33,8 @@ const StudyContainer = styled.div`
 `;
 
 const InnerContainer = styled.div`
-  width: 1200px;
-  margin-top: 80px;
+  width: 1280px;
+  margin-top: 99px;
   display: flex;
   flex-direction: column;
 `;

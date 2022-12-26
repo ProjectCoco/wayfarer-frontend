3e642e836ab9a385ae9e64135e9e440d5e69eb32
seonnn <ssni0105@gmail.com>
2022-12-26 23:26:18 +0900

    feat: CommunityPostDetailModal 추가 및 모달 open을 위한 state 추가

--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -3,18 +3,24 @@ import styled from "styled-components";
 import Banner from "../../components/common/Banner/Banner";
 import CommunityCardCarousel from "../../components/pages/Community/CommunityCards";
 import CommunityInput from "../../components/pages/Community/CommunityInput";
+import CommunityPostDetailModal from "../../components/pages/Community/CommunityModal/CommunityPostDetailModal";
 import CommunityPosts from "../../components/pages/Community/CommunityPosts";
 import CommunityTag from "../../components/pages/Community/CommunityTag";
 
 const Community = () => {
+  const [isModalOpened, setIsModalOpened] = useState(false);
+
   return (
-    <CommunityContainer>
+    <CommunityContainer isModalOpened={isModalOpened}>
+      {isModalOpened && (
+        <CommunityPostDetailModal setIsModalOpened={setIsModalOpened} />
+      )}
       <Banner text={"모집글 작성하기"} />
       <InnerContainer>
         <CommunityTag />
         <CommunityCardCarousel />
-        <CommunityInput />
-        <CommunityPosts />
+        <CommunityInput placeholder="혜민님, 어떤 이야기를 나누고 싶으세요?" />
+        <CommunityPosts setIsModalOpened={setIsModalOpened} />
       </InnerContainer>
     </CommunityContainer>
   );
@@ -22,7 +28,7 @@ const Community = () => {
 
 export default Community;
 
-const CommunityContainer = styled.div`
+const CommunityContainer = styled.div<{ isModalOpened: boolean }>`
   display: flex;
   justify-content: center;
   align-items: center;

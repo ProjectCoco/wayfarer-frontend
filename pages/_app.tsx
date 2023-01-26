8efa87441aa5ba66a23e4886a60179fdfb0b4767
JEONGHWANMIN <mimi0j0j@naver.com>
2023-01-26 23:19:16 +0900

    feat : modal context provider 연결

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
 import Footer from "../components/common/Footer/Footer";
 import Header from "../components/common/Header/Header";
+import { DialogProvider } from "../context/DialogContext";
 import { defaultTheme } from "../styles/DefaultTheme";
 import GlobalStyle from "../styles/GlobalStyle";
 
@@ -10,8 +11,10 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <ThemeProvider theme={defaultTheme}>
         <GlobalStyle />
-        <Header />
-        <Component {...pageProps} />
+        <DialogProvider>
+          <Header />
+          <Component {...pageProps} />
+        </DialogProvider>
         <Footer />
       </ThemeProvider>
     </>

13815eecc561d01e7df54dd1ae7891d346a5d3ad
seonnn <ssni0105@gmail.com>
2023-02-02 19:27:59 +0900

    feat: 기술 스택 파일 export를 위한 index.ts 파일 작성

--- public/TechStackImg/index.ts
+++ public/TechStackImg/index.ts
@@ -1,11 +1,79 @@
-import django from "./django.png";
-import vue from "./vue.png";
-import figma from "./figma.svg";
+import html from "./front/html.png";
+import css from "./front/css.png";
+import javascript from "./front/javascript.png";
+import typescript from "./front/typescript.png";
+import react from "./front/react.png";
+import vue from "./front/vue.png";
+import svelte from "./front/svelte.png";
+import angular from "./front/angular.png";
+import jquery from "./front/jquery.png";
+import nextjs from "./front/nextjs.png";
+import redux from "./front/redux.png";
+
+import java from "./back/java.png";
+import kotlin from "./back/kotlin.png";
+import nodejs from "./back/nodejs.png";
+import python from "./back/python.png";
+import mongodb from "./back/mongodb.png";
+import mysql from "./back/mysql.png";
+import mariadb from "./back/mariadb.png";
+import postgresql from "./back/postgresql.png";
+import redis from "./back/redis.png";
+import spring from "./back/spring.png";
+import springboot from "./back/springboot.png";
+import nestjs from "./back/nestjs.png";
+import django from "./back/django.png";
+import php from "./back/php.png";
+import docker from "./back/docker.png";
+
+import figma from "./design/figma.png";
+import sketch from "./design/sketch.png";
+import ai from "./design/ai.png";
+import ps from "./design/ps.png";
+import indesign from "./design/indesign.png";
+import ae from "./design/ae.png";
+
+import notion from "./notion.png";
+
+import flutter from "./mobile/flutter.png";
+import swift from "./mobile/swift.png";
 
 export const TechStackImg = {
-  django,
+  html,
+  css,
+  javascript,
+  typescript,
+  react,
   vue,
+  svelte,
+  angular,
+  jquery,
+  nextjs,
+  redux,
+  java,
+  kotlin,
+  nodejs,
+  python,
+  mongodb,
+  mysql,
+  mariadb,
+  postgresql,
+  redis,
+  spring,
+  springboot,
+  nestjs,
+  django,
+  php,
+  docker,
   figma,
+  sketch,
+  ai,
+  ps,
+  indesign,
+  ae,
+  notion,
+  flutter,
+  swift,
 };
 
 export type TechStackImgType = keyof typeof TechStackImg;

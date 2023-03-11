6878bdcb6d26b25e6cf1f5b867ba68dd709f21be
JEONGHWANMIN <ghksals09041@gmail.com>
2023-03-11 12:20:23 +0900

    Feat : 아톰 인풋 컴포넌트 추가

---
+++ components/atoms/AtomInput/AtomInput.tsx
@@ -0,0 +1,33 @@
+import React, { InputHTMLAttributes } from "react";
+import styled from "styled-components";
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  error?: boolean;
+}
+
+const AtomInput = ({ ...props }: InputProps) => {
+  return <StyledInput {...props} />;
+};
+
+export default AtomInput;
+
+const StyledInput = styled.input<{ error?: boolean }>`
+  border: none;
+  padding-bottom: 8px;
+  color: ${({ error }) => (error ? "#F94A4A" : "#222222")};
+  border-bottom: ${({ error }) =>
+    error ? "solid 1px #F94A4A" : "solid 1px #c2c2c2"};
+
+  &::placeholder {
+    font-weight: 500;
+    font-size: 15px;
+    line-height: 18px;
+    letter-spacing: -0.03em;
+    color: "#222222";
+    opacity: 0.3;
+  }
+
+  &:focus {
+    outline: none;
+  }
+`;
